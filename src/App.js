04f5297203f53e759e16b7bd9a9c234727304ef2
BahamaDev <41267607+BahamaDev@users.noreply.github.com>
2022-07-18 03:28:28 -0400

    sidebar added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import { useState, useEffect } from "react";
 import SavedData from "./components/SavedData";
 import NavBar from "./components/NavBar";
@@ -56,114 +56,129 @@ function App() {
   return (
     <>
       <NavBar />
-      {/* For Input Section */}
-      {/* For Input Section */}
-      <div className="container main-container  pt-5 pb-5 ">
-        <section className="row mx-auto" id="input-section">
-          {" "}
-          <div className="row mx-auto border justify-content-center">
-            {" "}
-            <div className="col-2 card text-center text-md-start pt-3">
-              {" "}
-              <label htmlFor="purchase-price">Purchase Price</label>
-              <input
-                className="form-control mb-2"
-                type="number"
-                id="purchase-price"
-                value={purchasePrice}
-                onChange={(e) => handlePurchaseChange(e)}
-              />
-              <label htmlFor="down-payment">Down Payment</label>
-              <input
-                type="text"
-                className="form-control mb-2 "
-                id="down-payment"
-                value={downPayment}
-                onChange={(e) => handlDownPaymentChange(e)}
-              />
-              <label htmlFor="interest-rate">Interest Rate</label>
-              <input
-                type="text"
-                className="form-control mb-2"
-                id="interest-rate"
-                value={interestRate}
-                onChange={(e) => handleInterestChange(e)}
-              />
-              <label htmlFor="payment-period">Payment Period</label>
-              <input
-                type="text"
-                className="form-control mb-2"
-                id="payment-period"
-                value={paymentPeriod}
-                onChange={(e) => handlePeriodChange(e)}
-              />
-              <button
-                className="btn btn-primary mb-2"
-                type="submit"
-                onClick={getMortgage}
-              >
-                Calculate
-              </button>
-            </div>
-            {/* For Output Section */}
-            {/* For Output Section */}
-            <div className="col-6  card">
-              <div className="row mx-auto text-center mb-2 mt-2">
-                Your Mortgage
-              </div>
-              <div className="row mtg-data border-top text-center ">
-                <div className="col-md-4 border-bottom h5">
-                  Principal: ${data.principal}
-                </div>
-                <div className="col-md-4 col-sm-6 border-bottom h5">
-                  Interest: {data.interest}%{" "}
-                </div>
-                <div className="col-md-4 col-sm-6 border-bottom h5">
-                  Period: {paymentPeriod} yrs{" "}
-                </div>
-              </div>
-              <div className="jumbotron">
-                <p className="h2 text-center">${data.monthly}</p>
-              </div>
-              <button className="btn btn-secondary" onClick={saveQuote}>
-                {" "}
-                Save Quote
-              </button>
-            </div>
-          </div>
-        </section>
-        {/* For Output Section */}
-        {/* For Output Section */}
 
-        {/* <section className="row pt-3">
-          <div className="col-8 mx-auto card">
-            <div className="row mx-auto text-center mb-2 mt-2">
-              Your Mortgage
-            </div>
-            <div className="row mtg-data border-top text-center ">
-              <div className="col-md-4 border-bottom h5">
-                Principal: ${data.principal}
-              </div>
-              <div className="col-md-4 col-sm-6 border-bottom h5">
-                Interest: {data.interest}%{" "}
-              </div>
-              <div className="col-md-4 col-sm-6 border-bottom h5">
-                Period: {paymentPeriod} yrs{" "}
-              </div>
-            </div>
-            <div className="jumbotron">
-              <p className="h2 text-center">${data.monthly}</p>
-            </div>
-            <button className="btn btn-secondary" onClick={saveQuote}>
-              {" "}
-              Save Quote
-            </button>
+      <div className="container mx-auto align-items-center page-container">
+        {" "}
+        <div className="row  main-row">
+          {/* Sidebar 1 */}
+          <div className="col border side border-dark ">
+            <div className="h3">HELLO</div>
           </div>
-        </section> */}
-      </div>{" "}
-      <section className="container">
-        <SavedData saved={saved} />
-      </section>
+
+          {/* Middle Column / Main Content */}
+          <div className="col-8 border border-primary">
+            {" "}
+            <div className="container pt-5 pb-5 ">
+              <header className="row pb-3">
+                <div className="col text-center mx-auto ">
+                  <h1>Free Mortgage Calculator</h1>
+                </div>
+              </header>
+
+              {/* Input and Output Sections */}
+              <section className="row mx-auto" id="input-section">
+                {" "}
+                <div className="row mx-auto gap-2 justify-content-center">
+                  {" "}
+                  {/* For Input Section */}
+                  {/* For Input Section */}
+                  <div className="col-md-3 card text-center text-md-start pt-3">
+                    {" "}
+                    <label htmlFor="purchase-price">Purchase Price</label>
+                    <input
+                      className="form-control mb-2"
+                      type="number"
+                      id="purchase-price"
+                      value={purchasePrice}
+                      onChange={(e) => handlePurchaseChange(e)}
+                    />
+                    <label htmlFor="down-payment">Down Payment</label>
+                    <input
+                      type="text"
+                      className="form-control mb-2 "
+                      id="down-payment"
+                      value={downPayment}
+                      onChange={(e) => handlDownPaymentChange(e)}
+                    />
+                    <label htmlFor="interest-rate">Interest Rate</label>
+                    <input
+                      type="text"
+                      className="form-control mb-2"
+                      id="interest-rate"
+                      value={interestRate}
+                      onChange={(e) => handleInterestChange(e)}
+                    />
+                    <label htmlFor="payment-period">Payment Period</label>
+                    <input
+                      type="text"
+                      className="form-control mb-2"
+                      id="payment-period"
+                      value={paymentPeriod}
+                      onChange={(e) => handlePeriodChange(e)}
+                    />
+                    <button
+                      className="btn btn-primary mb-2"
+                      type="submit"
+                      onClick={getMortgage}
+                    >
+                      Calculate
+                    </button>
+                  </div>
+                  {/* For Output Section */}
+                  {/* For Output Section */}
+                  <div className="col-6 justify-content-center card">
+                    <div className="row mx-auto text-center">Your Mortgage</div>
+                    <div className="row mtg-data border-top text-center ">
+                      <div className="col-md-4 border-bottom h5">
+                        Principal: ${data.principal}
+                      </div>
+                      <div className="col-md-4 col-sm-6 border-bottom h5">
+                        Interest: {data.interest}%{" "}
+                      </div>
+                      <div className="col-md-4 col-sm-6 border-bottom h5">
+                        Period: {paymentPeriod} yrs{" "}
+                      </div>
+                    </div>
+                    <div className="">
+                      <p className=" h6 text-center">Total monthly</p>
+                      <p className="h2 text-center">${data.monthly} </p>
+                    </div>
+                    <button className="btn btn-secondary" onClick={saveQuote}>
+                      {" "}
+                      Save Quote
+                    </button>
+                  </div>
+                </div>
+              </section>
+
+              {/* Ad Space */}
+              <section className="row pt-3 pb-3">
+                <div className="col-md-9 border mx-auto text-center h3 mid-banner">
+                  AD SPACE
+                </div>
+              </section>
+
+              {/* Saved Data */}
+              <section className="row ">
+                <div className="col">
+                  <SavedData saved={saved} />
+                </div>
+              </section>
+
+              <section className="row pt-3 pb-3 align-baseline">
+                <div className="col-9 border mx-auto text-center h3 mid-banner">
+                  AD SPACE
+                </div>
+              </section>
+            </div>{" "}
+          </div>
+
+          {/* Sidebar 2 */}
+          <div className="col border side border-dark ">
+            <div className="h3">WORLD</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
